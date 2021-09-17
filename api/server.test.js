c499aea6c80a4c6501ff6f3f1105b92989dba5da
JackOrth <jackorth19@gmail.com>
2021-09-17 14:49:07 -0600

    Completed tests for login and register.

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,32 @@
-// Write your tests here
+const request = require('supertest')
+const db = require('../data/dbConfig')
+const server = require ('../api/server')
+
+beforeAll(async () => {
+    await db.migrate.rollback()
+    await db.migrate.latest()
+})
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
+})
+
+describe('[POST] /register', () => {
+  it('should return a 201 OK Status', async () => {
+    const res = await request(server).post('/api/auth/register').send({username: 'Jack', password: 'password'})
+    expect(res.status).toBe(201)
+  })
+  it('responds with a 401 if no payload', async () => {
+    const res = await request(server).post('/api/auth/register').send({})
+    expect(res.status).toBe(401)
+  })
+})
+describe('[POST] /login', () => {
+  it('returns a status 401 if credential invalid', async () => {
+    const res = await request(server).post('/api/auth/login').send({username: 'invalid', password: 'credentials'})
+    expect(res.status).toBe(401)
+  })
+  it('responds with a  if credential invalid', async () => {
+    const res = await request(server).post('/api/auth/login').send({username: 'invalid', password: 'credentials'})
+    expect(res.body.message).toBe('Invalid credentials')
+  })
 })
